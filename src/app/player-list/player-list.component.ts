3d12ae1270d60ac19226b8c36b314aa48611262d
“BassamSalibi” <W0863179@myscc.ca>
2024-09-25 13:01:23 -0400

    completed step 10

--- src/app/player-list/player-list.component.ts
+++ src/app/player-list/player-list.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {Player} from "../Models/Player";
 import {PlayerListItemsComponent} from "../player-list-items/player-list-items.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-player-list',
   standalone: true,
   imports: [
-    PlayerListItemsComponent
+    PlayerListItemsComponent,
+    NgForOf
   ],
   templateUrl: './player-list.component.html',
   styleUrl: './player-list.component.css'
@@ -20,6 +22,6 @@ export class PlayerListComponent {
   Player5: Player = {id: 5, firstName: 'Kevin', lastName: 'De Bruyne', jerseyNumber: 17 , isTeamLeader: false};
   Player6: Player = {id: 6, firstName: 'Robert', lastName: 'Lewandowski', jerseyNumber: 9};
 
-  PlayerList: Player[] = [this.Player1, this.Player2, this.Player3, this.Player4, this.Player5, this.Player6 ];
+  PlayerList: Player[] = [this.Player1, this.Player2, this.Player3, this.Player4, this.Player5, this.Player6];
 
 }
