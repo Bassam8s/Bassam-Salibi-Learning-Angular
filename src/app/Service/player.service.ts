9fcf83155428cab011afcfad5b9e18d86e0004b4
“BassamSalibi” <W0863179@myscc.ca>
2024-10-06 16:15:27 -0400

    Completed Steps 8,9

--- src/app/Service/player.service.ts
+++ src/app/Service/player.service.ts
@@ -18,6 +18,29 @@ export class PlayerService {
   getPlayers(): Observable<Player[]> {
     return of(PlayerList); // Return and Observable
   }
-
   //Adding basic CRUD methods
+  // Creat : Add Player
+  addPlayer(newPlayer: Player): Observable<Player[]>{
+    this.players.push(newPlayer);
+    return of(this.players)
+  }
+
+  // Update Player
+  updatePlayer(updatedPlayer: Player): Observable<Player[]> {
+    const index = this.players.findIndex(player => player.id === updatedPlayer.id);
+    if (index !== -1) {
+      this.players[index] = updatedPlayer;
+    }
+    return of(this.players);
+  }
+
+  // Delete : Remove a player by ID
+  deletePlayer(playerId: number): Observable<Player[]> {
+    this.players = this.players.filter(player => player.id !== playerId);
+    return of(this.players);
+  }
+  getPlayerById(playerId: number): Observable<Player | undefined>  {
+    const player = this.players.find(player => player.id === playerId);
+    return of(player);
+  }
 }
--- src/app/player-list/player-list.component.ts
+++ src/app/player-list/player-list.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Player} from "../Shared/Models/Player";
 import {PlayerListItemsComponent} from "../player-list-items/player-list-items.component";
 import {NgClass, NgForOf, NgStyle} from "@angular/common";
+import {PlayerService} from "../Service/player.service";
 
 @Component({
   selector: 'app-player-list',
@@ -15,7 +16,24 @@ import {NgClass, NgForOf, NgStyle} from "@angular/common";
   templateUrl: './player-list.component.html',
   styleUrl: './player-list.component.css'
 })
-export class PlayerListComponent {
+export class PlayerListComponent implements OnInit {
+  // Placeholder for all player-list-items values
+  displayedColumns: string[] = ['id' , 'firstName', 'jerseyNumber']
   PlayerList : Player[] = [];
-}
 
+  constructor(private playerService: PlayerService) {
+    // This constructor is primarily used for injection
+  }
+  ngOnInit() {
+    // fetch and init our data
+    this.playerService.getPlayers().subscribe({
+      next: (data: Player[]) => this.PlayerList = data,
+      error: err => console.error("Error fetching Students", err),
+      complete:() => console.log("Student data fetch complete!")
+    });
+  }
+  selectedPlayer?: Player;
+  selectePlayer(player: Player): void{
+    this.selectedPlayer = player;
+}
+}
